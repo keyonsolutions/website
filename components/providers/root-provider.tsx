702d42237ee0ffc12e5d431294d147b0f0f876a1
Felix Strohmeier <felix.strohmeier@keyon.solutions>
2026-09-25 21:17:10 +0200

    feat: add vercel analytics

--- components/providers/root-provider.tsx
+++ components/providers/root-provider.tsx
@@ -1,4 +1,6 @@
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootProvider({
   children,
@@ -6,13 +8,18 @@ export default function RootProvider({
   children: React.ReactNode;
 }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <>
+      <Analytics />
+      <SpeedInsights />
+
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
   );
 }
